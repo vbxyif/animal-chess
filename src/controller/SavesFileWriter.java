1e15d410cc76a84e7babde109067e6919a3dedfa
vbxyif <z2384089564@outlook.com>
2023-05-23 19:29:35 +0800

    变更

--- src/controller/SavesFileWriter.java
+++ src/controller/SavesFileWriter.java
@@ -6,10 +6,6 @@ import java.io.IOException;
 
 public class SavesFileWriter extends FileWriter {
 
-    private SavesFileWriter(String fileName, boolean append) throws IOException {
-        super(fileName, append);
-    }
-
     public SavesFileWriter(File file) throws IOException {
         super(file);
     }
